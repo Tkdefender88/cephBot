c01d32a94d7c9bf017ff0ab0db67f4b0d516588a
Tkdefender88 <tkdefender88@gmail.com>
2018-01-10 00:52:58 -0700

    added comments and animated bigMojis

--- bot/command_methods.go
+++ bot/command_methods.go
@@ -7,6 +7,8 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+//ping is basically the hello world test of this whole monstrosity... it worked and now we have lots more
+//it sees if the message is a ping it pongs and vicea versa
 func ping(s *discordgo.Session, m *discordgo.MessageCreate, message []string) {
 	if message[0] == "ping" {
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
@@ -15,19 +17,27 @@ func ping(s *discordgo.Session, m *discordgo.MessageCreate, message []string) {
 	}
 }
 
+//msgHelp displays the help messages for the commands of the bot
+//if there is no command specified as an argument for the help command then
+//it lists all the commands the bot currently knows.
 func msgHelp(s *discordgo.Session, m *discordgo.MessageCreate, msgList []string) {
+	//checks for an arg specifying a command for help
 	if len(msgList) == 2 {
 		if val, ok := commandMap[toLower(msgList[1])]; ok {
+			//once the string is matched to a command display that commands help string
 			val.helpMessage(s, m)
 			return
 		}
+		//if the string is not matched then we inform them and then print the list of commands
+		s.ChannelMessageSend(m.ChannelID, msgList[1]+" is not a command I know, sorry")
 	}
-
+	//create an list of the commands and populate it with each command name
 	var commands []string
 	for _, val := range commandMap {
-		commands = append(commands, "`"+val.Name+"`")
+		commands = append(commands, "`"+val.Name+"`") //the back tics create code blocks in discord markdown
 	}
 
+	//SEND THE LIST OF ALL COMMANDS WOO!
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
 		Color: config.EmbedColor,
 
@@ -40,6 +50,7 @@ func msgHelp(s *discordgo.Session, m *discordgo.MessageCreate, msgList []string)
 	})
 }
 
+//gitHubLink this is literally as it reads. It posts a link to my github repo for the is repo to chat
 func gitHubLink(s *discordgo.Session, m *discordgo.MessageCreate, _ []string) {
 	s.ChannelMessageSend(
 		m.ChannelID,
@@ -47,6 +58,7 @@ func gitHubLink(s *discordgo.Session, m *discordgo.MessageCreate, _ []string) {
 			"\nLeave a star and make Juicetin's day! :star:")
 }
 
+//celebration is a command just for fun that cheers everyone up and gets the party started! woo!
 func celebration(s *discordgo.Session, m *discordgo.MessageCreate, _ []string) {
 	s.ChannelMessageSend(
 		m.ChannelID,
