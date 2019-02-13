3b7ae1d9aa3b0c068e9edd1f689333cd56da4eef
TKDefender <tkdefender88@gmail.com>
2019-02-12 23:58:59 -0700

    fun

--- bot/commandMethods.go
+++ bot/commandMethods.go
@@ -32,7 +32,7 @@ func init() {
 }
 
 func banUsr(s *discordgo.Session, m *discordgo.MessageCreate, msg []string) {
-	uID := "543623628638650368"
+	uID := "229238470957203456"
 	guild, err := guildDetails("520344973032423437", s)
 	if err != nil {
 		log.Println(err.Error())
